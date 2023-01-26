c1a3bb853c7737235ee5e14095cc97fa357d0ec5
Jeyaprabakar01 <ej.prabakar@gmail.com>
2023-01-26 14:44:59 +0530

    gallery updated with map function

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,35 +5,35 @@ const Gallery = () => {
   const cardDetails = [
     {
       title: "deep earth",
-      img: "bg-img0-sm xl:bg-img0-xl",
+      classNames: "bg-img0-sm xl:bg-img0-xl",
     },
     {
       title: "night arcade",
-      img: "bg-img1-sm xl:bg-img1-xl",
+      classNames: "bg-img1-sm xl:bg-img1-xl",
     },
     {
       title: "soccer team vr",
-      img: "bg-img2-sm xl:bg-img3-xl",
+      classNames: "bg-img2-sm xl:bg-img3-xl",
     },
     {
       title: "the grid",
-      img: "bg-img3-sm xl:bg-img3-xl",
+      classNames: "bg-img3-sm xl:bg-img3-xl",
     },
     {
       title: "from up above vr",
-      img: "bg-img4-sm xl:bg-img4-xl",
+      classNames: "bg-img4-sm xl:bg-img4-xl",
     },
     {
       title: "pocket porealis",
-      img: "bg-img5-sm xl:bg-img5-xl",
+      classNames: "bg-img5-sm xl:bg-img5-xl",
     },
     {
       title: "the curiosity",
-      img: "bg-img6-sm xl:bg-img6-xl",
+      classNames: "bg-img6-sm xl:bg-img6-xl",
     },
     {
       title: "make it fisheye",
-      img: "bg-img7-sm xl:bg-img7-xl",
+      classNames: "bg-img7-sm xl:bg-img7-xl",
     },
   ];
 
@@ -53,14 +53,9 @@ const Gallery = () => {
           className="flex flex-col gap-y-4
                       xl:grid xl:grid-cols-4 xl:gap-6 mt-20"
         >
-          <Card title="deep earth" img="bg-img0-sm xl:bg-img0-xl" />
-          <Card title="night arcade" img="bg-img1-sm xl:bg-img1-xl" />
-          <Card title="soccer team vr" img="bg-img2-sm xl:bg-img2-xl" />
-          <Card title="the grid" img="bg-img3-sm xl:bg-img3-xl" />
-          <Card title="from up above vr" img="bg-img4-sm xl:bg-img4-xl" />
-          <Card title="pocket porealis" img="bg-img5-sm xl:bg-img5-xl" />
-          <Card title="the curiosity" img="bg-img6-sm xl:bg-img6-xl" />
-          <Card title="make it fisheye" img="bg-img7-sm xl:bg-img7-xl" />
+          {cardDetails.map((detail) => {
+            return <Card title={detail.title} img={detail.classNames} />;
+          })}
         </div>
 
         <div className="flex justify-center xl:hidden mt-12 ">
