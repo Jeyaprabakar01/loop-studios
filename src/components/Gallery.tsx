5df541a3e46257da18fe5c7cf4f59c35f2d60fff
Jeyaprabakar01 <ej.prabakar@gmail.com>
2023-01-26 11:51:52 +0530

    components updated

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,4 +1,6 @@
+import { title } from "process";
 import Card from "./Card";
+import Section from "./Section";
 
 const Gallery = () => {
   const cardDetails = [
@@ -37,36 +39,38 @@ const Gallery = () => {
   ];
 
   return (
-    <section className="px-6 pt-4 xl:px-40 xl:pt-16 mb-40">
-      <div className="flex md:justify-between justify-center">
-        <h1 className="md:text-5xl text-4xl uppercase font-light">
-          Our Creations
-        </h1>
-        <button className="hidden md:block uppercase tracking-widest border-2 border-gray-900 w-36 hover:bg-black hover:text-white duration-200 transition">
-          See All
-        </button>
-      </div>
+    <Section>
+      <section className="px-6 pt-4 xl:px-40 xl:pt-16 mb-40">
+        <div className="flex md:justify-between justify-center">
+          <h1 className="md:text-5xl text-4xl uppercase font-light">
+            Our Creations
+          </h1>
+          <button className="hidden md:block uppercase tracking-widest border-2 border-gray-900 w-36 hover:bg-black hover:text-white duration-200 transition">
+            See All
+          </button>
+        </div>
 
-      <div
-        className="flex flex-col gap-y-4
+        <div
+          className="flex flex-col gap-y-4
                       xl:grid xl:grid-cols-4 xl:gap-6 mt-20"
-      >
-        <Card title="deep earth" img="bg-img0-sm xl:bg-img0-xl" />
-        <Card title="night arcade" img="bg-img1-sm xl:bg-img1-xl" />
-        <Card title="soccer team vr" img="bg-img2-sm xl:bg-img2-xl" />
-        <Card title="the grid" img="bg-img3-sm xl:bg-img3-xl" />
-        <Card title="from up above vr" img="bg-img4-sm xl:bg-img4-xl" />
-        <Card title="pocket porealis" img="bg-img5-sm xl:bg-img5-xl" />
-        <Card title="the curiosity" img="bg-img6-sm xl:bg-img6-xl" />
-        <Card title="make it fisheye" img="bg-img7-sm xl:bg-img7-xl" />
-      </div>
+        >
+          <Card title="deep earth" img="bg-img0-sm xl:bg-img0-xl" />
+          <Card title="night arcade" img="bg-img1-sm xl:bg-img1-xl" />
+          <Card title="soccer team vr" img="bg-img2-sm xl:bg-img2-xl" />
+          <Card title="the grid" img="bg-img3-sm xl:bg-img3-xl" />
+          <Card title="from up above vr" img="bg-img4-sm xl:bg-img4-xl" />
+          <Card title="pocket porealis" img="bg-img5-sm xl:bg-img5-xl" />
+          <Card title="the curiosity" img="bg-img6-sm xl:bg-img6-xl" />
+          <Card title="make it fisheye" img="bg-img7-sm xl:bg-img7-xl" />
+        </div>
 
-      <div className="flex justify-center xl:hidden mt-12 ">
-        <button className="border-2 border-black tracking-widest uppercase font-semibold p-2 w-32 hover:bg-black hover:text-white duration-200 transition">
-          See All
-        </button>
-      </div>
-    </section>
+        <div className="flex justify-center xl:hidden mt-12 ">
+          <button className="border-2 border-black tracking-widest uppercase font-semibold p-2 w-32 hover:bg-black hover:text-white duration-200 transition">
+            See All
+          </button>
+        </div>
+      </section>
+    </Section>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import HamburgerIcon from "../assets/icon/icon-hamburger.svg";
+import Section from "./Section";
 
 const Header = () => {
   const navigation = ["About", "Careers", "Events", "Products", "Support"];
@@ -8,42 +9,44 @@ const Header = () => {
 
   return (
     <main className="bg-mobile-image-hero xl:bg-image-hero h-screen relative bg-cover ">
-      <div className="px-6 pt-4 xl:px-40 xl:pt-16">
-        <div className="flex items-center justify-between text-white font-['Alata']  ">
-          <h1 className="font-bold text-3xl xl:text-4xl">loopstudios</h1>
-          <img
-            src={HamburgerIcon}
-            alt=""
-            className="xl:hidden lg:hidden md:hidden z-20"
-            onClick={() => setMenu(!menu)}
-          />
+      <Section>
+        <div className="px-6 pt-4 xl:px-40 xl:pt-16">
+          <div className="flex items-center justify-between text-white font-['Alata']  ">
+            <h1 className="font-bold text-3xl xl:text-4xl">loopstudios</h1>
+            <img
+              src={HamburgerIcon}
+              alt=""
+              className="xl:hidden lg:hidden md:hidden z-20"
+              onClick={() => setMenu(!menu)}
+            />
 
-          <ul
-            className={`gap-10
+            <ul
+              className={`gap-10
                       ${
                         menu
                           ? "absolute gap-6 top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-black z-10"
                           : "hidden"
                       } xl:flex lg:flex md:flex`}
-          >
-            {navigation.map((nav, key) => {
-              return (
-                <li className="hover:underline cursor-pointer" key={key}>
-                  {nav}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+            >
+              {navigation.map((nav, key) => {
+                return (
+                  <li className="hover:underline cursor-pointer" key={key}>
+                    {nav}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
 
-        <div className="absolute right-1 bottom-80 xl:right-4 xl:bottom-48 mx-auto container flex justify-center xl:justify-start">
-          <article className="xl:p-10 p-6 xl:ml-44 xl:text-7xl text-4xl xl:w-2/5 w-4/5 border-white border-2">
-            <h1 className="uppercase font-light text-white">
-              immersive experiences that deliver
-            </h1>
-          </article>
+          <div className="absolute right-1 bottom-80 xl:right-4 xl:bottom-48 mx-auto container flex justify-center xl:justify-start">
+            <article className="xl:p-10 p-6 xl:ml-44 xl:text-7xl text-4xl xl:w-2/5 w-4/5 border-white border-2">
+              <h1 className="uppercase font-light text-white">
+                immersive experiences that deliver
+              </h1>
+            </article>
+          </div>
         </div>
-      </div>
+      </Section>
     </main>
   );
 };
